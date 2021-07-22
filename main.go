a2bd2caaafae71dccab69c884f34691eb6fdb607
pereiraJr <lielsonjr@gmail.com>
2021-07-22 20:47:55 -0300

    respeitando variavel de ambiente PORT

--- main.go
+++ main.go
@@ -3,15 +3,20 @@ package main
 import (
 	"log"
 	"net/http"
-
+  "os"
 	// "eddy.com/todo/data"
 	"eddy.com/todo/route"
 )
 
 func main() {
+	var PORT = ":9090"
+
+	if envPort := os.Getenv("PORT"); envPort != ""{
+		PORT = ":" + envPort
+	}
 
 	// data.InitDatabase()
 
-	log.Print("Server on")
-	http.ListenAndServe(":9000", route.RegisterRoute())
+	log.Print("Server on ", PORT)
+	http.ListenAndServe(PORT, route.RegisterRoute())
 }
